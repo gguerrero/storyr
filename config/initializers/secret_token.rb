cc964fa0357dca38620784391f898497a632db0f
Guillermo Guerrero <g.guerrero.bus@gmail.com>
2013-07-06 17:15:17 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Storyr::Application.config.secret_key_base = 'c9c21e570d9ca4e18e09eb50ae8dc7e8b10dfca254354a94a22d62b7ec809491d1fe3b79497bb88a14370ca3a406f12421461ac86a0874fe4e540f4bec9af256'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Storyr::Application.config.session_store :cookie_store, key: '_storyr_session'
